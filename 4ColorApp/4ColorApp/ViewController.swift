908a672e2ac5f212e0fb2951fe59bd890a79b27d
Salih Yusuf Göktaş <sygoktas@outlook.com.tr>
2023-03-09 15:39:53 +0300

    Auto Layout related mini app
    Auto Layout related mini app

---
+++ 4ColorApp/4ColorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4ColorApp
+//
+//  Created by Salih Yusuf Göktaş on 9.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
